5062efc4773b543c20d66b33bd3dcd2736c44b6c
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-23 11:36:51 -0700

    Update controller & User poro

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.new(request.env['omniauth.auth'])
-    session[:user] = user
+    session[:user] = user.raw_data
     redirect_to '/greenhouse'
   end
 end
--- app/poros/user.rb
+++ app/poros/user.rb
@@ -5,4 +5,13 @@ class User
     @email = data[:info][:email]
     @image = data[:info][:image]
   end
+
+  def raw_data
+    {
+      uid: @uid,
+      name: @name,
+      email: @email,
+      image: @image
+    }
+  end
 end
