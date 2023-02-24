27a89d6feea8d5e9f5b0f766ba6e80c913192d39
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-24 14:39:23 -0700

    fix: Refactor service routes by abstracting api/v1

--- app/services/fern_service.rb
+++ app/services/fern_service.rb
@@ -1,6 +1,6 @@
 class FernService
   def self.conn
-    Faraday.new(url: "http://localhost:3000")
+    Faraday.new(url: "http://localhost:3000/api/v1")
   end
 
   def self.parse_json(response)
@@ -9,35 +9,35 @@ class FernService
 
   # ferns
   def self.create_fern(google_id, fern_params)
-    conn.post("/api/v1/users/#{google_id}/ferns", fern_params)
+    conn.post("/users/#{google_id}/ferns", fern_params)
   end
 
   def self.get_all_ferns(google_id)
-    conn.get("/api/v1/users/#{google_id}/ferns")
+    conn.get("/users/#{google_id}/ferns")
   end
 
   def self.get_fern(google_id, fern_id)
-    conn.get("/api/v1/users/#{google_id}/ferns/#{fern_id}")
+    conn.get("/users/#{google_id}/ferns/#{fern_id}")
   end
 
   def self.update_fern(google_id, fern_id, fern_params)
-    conn.patch("/api/v1/users/#{google_id}/ferns/#{fern_id}", fern_params)
+    conn.patch("/users/#{google_id}/ferns/#{fern_id}", fern_params)
   end
 
   def self.delete_fern(google_id, fern_id)
-    conn.delete("/api/v1/users/#{google_id}/ferns/#{fern_id}")
+    conn.delete("/users/#{google_id}/ferns/#{fern_id}")
   end 
 
   # shelves
   def self.get_ferns_on_shelf(google_id, shelf_id)
-    conn.get("/api/v1/users/#{google_id}/shelves/#{shelf_id}/ferns")
+    conn.get("/users/#{google_id}/shelves/#{shelf_id}/ferns")
   end
 
   def self.add_fern_to_shelf(google_id, shelf_id, fern_id)
-    conn.post("/api/v1/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
+    conn.post("/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
   end
 
   def self.remove_fern_from_shelf(google_id, shelf_id, fern_id)
-    conn.delete("/api/v1/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
+    conn.delete("/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
   end
 end
