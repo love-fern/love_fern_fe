7918293b3bb9e2984e3b5b53b2ff7a3c61185693
J Seymour <JustJakeSeymour@gmail.com>
2023-02-24 16:21:38 -0700

    play around with tests

--- app/services/fern_service.rb
+++ app/services/fern_service.rb
@@ -1,10 +1,10 @@
 class FernService
   def self.conn
-    Faraday.new(url: "http://localhost:5000")
+    Faraday.new(url: "http://localhost:5000/api/v1/")
   end
 
   def self.find_shelves(google_id)
-    conn.get("/api/v1/users/#{google_id}/shelves")
+    conn.get("users/#{google_id}/shelves")
   end
 
   def self.parse_json(response)
@@ -13,12 +13,12 @@ class FernService
 
   # users
   def self.create_user(user_info)
-    conn.post("/users", user_info)
+    conn.post("users", user_info)
   end
   
   # ferns
   def self.create_fern(google_id, fern_params)
-    conn.post("/users/#{google_id}/ferns", fern_params)
+    conn.post("users/#{google_id}/ferns", fern_params)
   end
 
   def self.get_all_ferns(google_id)
@@ -26,27 +26,27 @@ class FernService
   end
 
   def self.get_fern(google_id, fern_id)
-    conn.get("/users/#{google_id}/ferns/#{fern_id}")
+    conn.get("users/#{google_id}/ferns/#{fern_id}")
   end
 
   def self.update_fern(google_id, fern_id, fern_params)
-    conn.patch("/users/#{google_id}/ferns/#{fern_id}", fern_params)
+    conn.patch("users/#{google_id}/ferns/#{fern_id}", fern_params)
   end
 
   def self.delete_fern(google_id, fern_id)
-    conn.delete("/users/#{google_id}/ferns/#{fern_id}")
+    conn.delete("users/#{google_id}/ferns/#{fern_id}")
   end 
 
   # shelves
   def self.get_ferns_on_shelf(google_id, shelf_id)
-    conn.get("/users/#{google_id}/shelves/#{shelf_id}/ferns")
+    conn.get("users/#{google_id}/shelves/#{shelf_id}/ferns")
   end
 
   def self.add_fern_to_shelf(google_id, shelf_id, fern_id)
-    conn.post("/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
+    conn.post("users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
   end
 
   def self.remove_fern_from_shelf(google_id, shelf_id, fern_id)
-    conn.delete("/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
+    conn.delete("users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
   end
 end
