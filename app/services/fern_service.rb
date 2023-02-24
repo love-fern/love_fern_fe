2d89e1cb3e5b9ea17b74099cfc3b08b864e0ad16
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-24 10:56:21 -0700

    feat: Map initial FernService to BE endpoints

--- app/services/fern_service.rb
+++ app/services/fern_service.rb
@@ -3,11 +3,41 @@ class FernService
     Faraday.new(url: "http://localhost:3000")
   end
 
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  # ferns
   def self.create_fern(google_id, fern_params)
     conn.post("/api/v1/users/#{google_id}/ferns", fern_params)
   end
 
-  def self.parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
+  def self.get_all_ferns(google_id)
+    conn.get("/api/v1/users/#{google_id}/ferns")
+  end
+
+  def self.get_fern(google_id, fern_id)
+    conn.get("/api/v1/users/#{google_id}/ferns/#{fern_id}")
+  end
+
+  def self.update_fern(google_id, fern_id, fern_params)
+    conn.patch("/api/v1/users/#{google_id}/ferns/#{fern_id}", fern_params)
+  end
+
+  def self.delete_fern(google_id, fern_id)
+    conn.delete("/api/v1/users/#{google_id}/ferns/#{fern_id}")
+  end 
+
+  # shelves
+  def self.get_ferns_on_shelf(google_id, shelf_id)
+    conn.get("/api/v1/users/#{google_id}/shelves/#{shelf_id}/ferns")
+  end
+
+  def self.add_fern_to_shelf(google_id, shelf_id, fern_id)
+    conn.post("/api/v1/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
+  end
+
+  def self.remove_fern_from_shelf(google_id, shelf_id, fern_id)
+    conn.delete("/api/v1/users/#{google_id}/shelves/#{shelf_id}/ferns/#{fern_id}")
   end
 end
