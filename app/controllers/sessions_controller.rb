0038bf5acbea5e72edf05b4ac14f2390a3504efb
Drew Layton <113313770+dlayton66@users.noreply.github.com>
2023-02-23 17:20:12 -0700

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.new(request.env['omniauth.auth'])
     session[:user] = user.raw_data
-    binding.pry
     redirect_to '/greenhouse'
   end
 end
