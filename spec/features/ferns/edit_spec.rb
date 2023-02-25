786752b8a49fe2333ef65f2cd2ded6ab1221045c
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-25 09:46:02 -0700

    fix: Change update_spec to edit_spec

---
+++ spec/features/ferns/edit_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Add Interaction To Fern", type: :feature do
+  describe 'Edit Fern', :vcr do
+    let(:user) { {
+      'uid' => "110920554030325122207",
+      'name' => "Samuel Cox",
+      'email' => "samc1253@gmail.com",
+      'image' => "https://lh3.googleusercontent.com/a/AGNmyxYt32X4YBRyuQij1sMMfHp6BbnKBs2Uaic2CLnLew=s96-c"
+    } }
+
+
+  it 'has the name of the fern at the top of the page' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit water_fern_path(fern)
+
+    expect(page).to have_content("Samuel Cox's Greenhouse")
+  end
+end
