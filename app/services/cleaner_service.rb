15cacaeca6768d432d44f864a8a2380d1252973b
Samuel Cox <samc1253@gmail.com>
2023-02-28 09:43:49 -0700

    Update cleaner_service.rb

--- app/services/cleaner_service.rb
+++ app/services/cleaner_service.rb
@@ -1,7 +1,7 @@
 class CleanerService
   def self.conn
     Faraday.new(
-      url: "#{ENV['url']}/api/v1/",
+      url: "#{ENV['URL']}/api/v1/",
       headers: { FErn_key: ENV['FErn_key'] }
     )
   end
