6fc36c92d86cd21676e6a23fe80bf640b1bcdfcf
Drew Layton <dlayton66@gmail.com>
2023-02-28 21:48:09 -0700

    test: Complete FernService tests

--- spec/services/fern_service_spec.rb
+++ spec/services/fern_service_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe FernService do
 
   describe '.create_fern' do
     it 'creates a new fern for a user', :vcr do
-      WebMock.disable!
       fern_params = {
         name: 'The Big Pepperoni',
         preferred_contact_method: 'text',
@@ -29,9 +28,44 @@ RSpec.describe FernService do
 
   describe '.get_all_ferns' do
     it 'gets all ferns for a user', :vcr do
-      WebMock.disable!
       response = FernService.get_all_ferns(user['uid'])
       expected = File.read('spec/fixtures/fern_service/get_all_ferns.json')
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq(JSON.parse(expected))
+    end
+  end
+
+  describe '.get_fern' do
+    it 'gets a fern for a user', :vcr do
+      response = FernService.get_fern(user['uid'], 2)
+      expected = File.read('spec/fixtures/fern_service/get_fern.json')
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq(JSON.parse(expected))    
+    end
+  end
+
+  describe '.update_fern' do
+    it 'updates a fern', :vcr do
+      fern_update_params = {
+        shelf: 'Business',
+        name: 'Fernilicious',
+        preferred_contact_method: "Don't" 
+      }
+
+      response = FernService.update_fern(user['uid'], 3, fern_update_params)
+      expected = File.read('spec/fixtures/fern_service/update_fern.json')
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq(JSON.parse(expected))
+    end
+  end
+
+  describe '.delete_fern' do
+    it 'deletes a fern', :vcr do
+      response = FernService.delete_fern(user['uid'], 4)
+      expected = File.read('spec/fixtures/fern_service/delete_fern.json')
 
       expect(response.status).to eq(200)
       expect(JSON.parse(response.body)).to eq(JSON.parse(expected))
