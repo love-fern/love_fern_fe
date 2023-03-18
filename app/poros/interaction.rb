a2202c4b166d2a1efb0948953009a99cf81f328d
Drew Layton <dlayton66@gmail.com>
2023-03-18 12:39:39 -0600

    feat/test: Add dynamic flash messages and emojis to sentiment analysis

--- app/poros/interaction.rb
+++ app/poros/interaction.rb
@@ -1,18 +1,57 @@
 class Interaction
-  attr_reader :positivity, :description, :created_at
+  attr_reader :evaluation, :description, :created_at
 
   def initialize(interaction_data)
     @id = interaction_data[:id]
-    @positivity = interaction_data[:attributes][:evaluation].downcase
+    @evaluation = interaction_data[:attributes][:evaluation]
     @description = interaction_data[:attributes][:description]
     @created_at = Date.parse(interaction_data[:attributes][:created_at])
   end
 
+  
+  def emoji
+    return "emojis/activity.png" unless @evaluation
+    "emojis/#{message_level}.png"
+  end
+  
+  def water_message
+    case message_level
+    when 7
+      "Wow! Your fern is speechless! That's just about the nicest thing anyone's ever said to it."
+    when 6
+      "Your fern is beaming! That was awfully kind of you to say."
+    when 5
+      "Your fern liked that. It's feeling a bit better now."
+    when 4
+      "Hmm... Your fern doesn't know what to think about that."
+    when 3
+      "Oof. Your fern didn't much care for that."
+    when 2
+      "Yikes! Your fern just died a little inside."
+    when 1
+      "The cruelty of man knows no bounds. Your fern is deeply hurt by these words."
+    end
+  end
+  
   def display_message
     if @description == 'message'
-      "You exchanged #{@positivity} words #{days_ago}."
+      "You exchanged #{positivity} words #{days_ago}."
     else
       "You decided to #{@description[0].downcase + @description[1..-1]} #{days_ago}."
+    end
+  end
+  
+  private
+  
+  def message_level
+    case @evaluation
+    when -0.25..0.25 then 4
+    when 0.25..0.50 then 5
+    when 0.50..0.75 then 6
+    when 0.75..1 then 7
+    when -0.50..-0.25 then 3
+    when -0.75..-0.50 then 2
+    when -1..-0.75 then 1
     end
   end
 
@@ -24,6 +63,17 @@ class Interaction
       'yesterday'
     else
       "#{(today - @created_at).to_i} days ago"
+    end
+  end
+
+  def positivity
+    case message_level
+    when 5..7
+      "positive"
+    when 1..3
+      "negative"
+    else
+      "neutral"
     end
   end
 end
--- spec/features/ferns/show_spec.rb
+++ spec/features/ferns/show_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Fern Show', type: :feature do
+  let(:user) do
+    {
+      'uid' => '110920554030325122207',
+      'name' => 'Samuel Cox',
+      'email' => 'samc1253@gmail.com',
+      'image' => 'https://lh3.googleusercontent.com/a/AGNmyxYt32X4YBRyuQij1sMMfHp6BbnKBs2Uaic2CLnLew=s96-c'
+    }
+  end
+
+  let(:user_2) do
+    {
+      'uid' => '113234860329276513988',
+      'name' => 'Anthony Ongaro',
+      'email' => 'aongaro@gmail.com',
+      'image' =>
+       'https://lh3.googleusercontent.com/a/AGNmyxZaV6gpWLtMVqa4RCcgDsiigEijEnmEviTX2mhQ1Q=s96-c',
+      'google_id' => '113234860329276513988'
+    }
+  end
+
   describe 'Fern Show Page', :vcr do
-    let(:user) do
-      {
-        'uid' => '110920554030325122207',
-        'name' => 'Samuel Cox',
-        'email' => 'samc1253@gmail.com',
-        'image' => 'https://lh3.googleusercontent.com/a/AGNmyxYt32X4YBRyuQij1sMMfHp6BbnKBs2Uaic2CLnLew=s96-c'
-      }
-    end
-
-    let(:user_2) do
-      {
-        'uid' => '113234860329276513988',
-        'name' => 'Anthony Ongaro',
-        'email' => 'aongaro@gmail.com',
-        'image' =>
-         'https://lh3.googleusercontent.com/a/AGNmyxZaV6gpWLtMVqa4RCcgDsiigEijEnmEviTX2mhQ1Q=s96-c',
-        'google_id' => '113234860329276513988'
-      }
-    end
-
     it 'Shows the fern information' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit fern_path(2)
@@ -34,6 +34,63 @@ RSpec.describe 'Fern Show', type: :feature do
       expect(page).to have_button('Water Fern')
       expect(page).to have_button('Compost Fern')
       expect(page).to have_button('Fertilize Fern')
+    end
+
+    it 'displays the last 3 interactions' do
+      # VCR.use_cassette("Fern_Show/Fern_Show_Page/displays_the_last_3_interactions.yml") do |cassette|
+      #   puts cassette.send(:raw_cassette_bytes)
+      # end
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit fern_path(2)
+  
+      binding.pry
+  
+      within '#last_3_interactions' do
+        expect(page).to have_content('Last Three Interactions:')
+      end
+      
+      click_button 'Water Fern'
+      fill_in :interaction, with: 'I watered this fern today. I love pizza. I love you.'
+      click_button 'Water Fern'
+  
+      within '#last_3_interactions' do
+        images = page.all('img')
+  
+        expect(images[0][:src]).to eq("/assets/emojis/6-0a948fb5518898d7aecf8c8ce0a071241bd278244434ada1a9036ffa9c642c53.png")
+        expect(page).to have_content("You exchanged positive words today.")
+      end
+  
+      click_button 'Water Fern'
+      fill_in :interaction, with: 'You Suck, I hate you'
+      click_button 'Water Fern'
+  
+      within '#last_3_interactions' do
+        images = page.all('img')
+  
+        expect(images[0][:src]).to eq("/assets/emojis/1-b0fd85cc734951271f10961a1ffd8275ab4246652c59e3550c91c19a6a4e4028.png")
+        expect(page).to have_content("You exchanged negative words today.")
+  
+        expect(images[1][:src]).to eq("/assets/emojis/6-0a948fb5518898d7aecf8c8ce0a071241bd278244434ada1a9036ffa9c642c53.png")
+        expect(page).to have_content("You exchanged positive words today.")
+      end
+  
+      visit fertilize_fern_path(2)
+      click_button 'Sure!'
+      
+      within '#last_3_interactions' do
+        images = page.all('img')
+  
+        expect(images[0][:src]).to eq("/assets/emojis/activity-73fdd286e8c3022d2cac58706016da71ae5588146cfbeeda3a4afb549f0840cd.png")
+        expect(page).to have_content("You decided to ") # test for activity dynamically
+        expect(page).to have_content(" today.")
+  
+        expect(images[1][:src]).to eq("/assets/emojis/1-b0fd85cc734951271f10961a1ffd8275ab4246652c59e3550c91c19a6a4e4028.png")
+        expect(page).to have_content("You exchanged negative words today.")
+  
+        expect(images[2][:src]).to eq("/assets/emojis/6-0a948fb5518898d7aecf8c8ce0a071241bd278244434ada1a9036ffa9c642c53.png")
+        expect(page).to have_content("You exchanged positive words today.")
+      end
     end
 
     it 'Will not navigate if the user is not logged in' do
@@ -51,15 +108,6 @@ RSpec.describe 'Fern Show', type: :feature do
   end
 
   describe 'Fern Delete', :vcr do
-    let(:user) do
-      {
-        'uid' => '110920554030325122207',
-        'name' => 'Samuel Cox',
-        'email' => 'samc1253@gmail.com',
-        'image' => 'https://lh3.googleusercontent.com/a/AGNmyxYt32X4YBRyuQij1sMMfHp6BbnKBs2Uaic2CLnLew=s96-c'
-      }
-    end
-
     it 'Can Compost a fern' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -80,40 +128,5 @@ RSpec.describe 'Fern Show', type: :feature do
       end
     end
 
-    it 'displays the last 3 interactions' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit fern_path(2)
-
-      within '#last_3_interactions' do
-        expect(page).to have_content('Last Three Interactions:')
-      end
-
-      click_button 'Water Fern'
-      fill_in :interaction, with: 'I watered this fern today. I love pizza. I love you.'
-      click_button 'Water Fern'
-
-      within '#last_3_interactions' do
-        expect(page).to have_content("You exchanged positive words today.")
-      end
-
-      click_button 'Water Fern'
-      fill_in :interaction, with: 'You Suck, I hate you'
-      click_button 'Water Fern'
-
-      within '#last_3_interactions' do
-        expect(page).to have_content("You exchanged negative words today.")
-        expect(page).to have_content("You exchanged positive words today.")
-      end
-
-      visit fertilize_fern_path(2)
-      click_button 'Sure!'
-
-      within '#last_3_interactions' do
-        expect(page).to have_content("You decided to ") # test for activity dynamically
-        expect(page).to have_content(" today.")
-        expect(page).to have_content("You exchanged negative words today.")
-        expect(page).to have_content("You exchanged positive words today.")
-      end
-    end
   end
 end
